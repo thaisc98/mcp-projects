d82156265741bab95f034d84e98cf082cea4b3e0
thaisc98 <thaisc98@hotmail.es>
2026-02-10 22:01:53 -0400

    adding data with psot reques tin fastapi

--- fast-api-mcp-todo/main.py
+++ fast-api-mcp-todo/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+fake_db = []
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
 
 
 @app.get("/")
@@ -14,3 +22,12 @@ async def get_weather():
         "temperature": 20,
         "description": "sunny"
     }
+
+@app.post("/items")
+async def create_item(item: Item):
+    fake_db.append(item.dict())
+    return {"message": "Item created successfully"}
+
+@app.get("/items")
+async def get_items():
+    return {"items": fake_db}
