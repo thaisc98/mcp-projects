dbbfd4e34f580e5a367db2ed8d813acf1c33797f
thaisc98 <thaisc98@hotmail.es>
2026-03-16 19:41:07 -0400

    [init]

--- fast-api-mcp-todo/main.py
+++ fast-api-mcp-todo/main.py
@@ -13,7 +13,7 @@ from typing import List, Optional
 import sqlite3
 from contextlib import contextmanager
 
-import uvicorn
+# import uvicorn
 from fastapi_mcp import FastApiMCP
 
 app = FastAPI(title="Simple Todo API", description="Basic Todo list management API using FastAPI + SQLite",version="1.0.0")
@@ -168,5 +168,5 @@ mcp = FastApiMCP(
 mcp.mount_http()  # exposes GET/POST at /mcp (Streamable HTTP transport)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
